8d2a3aef1cce359b9fb8338bd29a312597e71000
Efere0n <timvortex1220@gmail.com>
2024-11-02 22:09:06 +0200

    Lab1

---
+++ Lab1.sql
@@ -0,0 +1,21 @@
+1. 
+SELECT *
+FROM cities
+ORDER BY population desc
+LIMIT 10 OFFSET 11;
+2.
+select *
+from cities
+order by name desc
+limit 30;
+3.
+SELECT *
+FROM cities
+order by region, population desc;;
+4.
+SELECT distinct region
+FROM cities;
+5.
+select *
+from cities
+order by region desc, name desc;
